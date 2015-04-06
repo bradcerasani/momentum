9dc0ebfd3f9c0fe534f2e9259374ac1f25ee1c5a
Brad Cerasani <bradcerasani@gmail.com>
2015-04-05 23:04:11 -0500

    Refactor request function

--- app.js
+++ app.js
@@ -6,20 +6,28 @@ var fs = require('fs');
 var username = "bradcerasani";
 var url = "https://github.com/users/" + username + "/contributions";
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    parseString(body, function(err, result) {
-      var calendar = result.svg.g[0].g;
-      var currentWeek = calendar[calendar.length - 1].rect;
-      var currentDay = currentWeek[currentWeek.length - 1].$;
-      var resultString = JSON.stringify(currentDay, null, 2);
+function writeFile(contents) {
+  fs.writeFile("response.json", contents, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Today's contributions saved to response.json");
+  });
+}
 
-      fs.writeFile("response.json", resultString, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("Today's contributions saved to response.json");
+function getData(url) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      parseString(body, function(err, result) {
+        var calendar = result.svg.g[0].g;
+        var currentWeek = calendar[calendar.length - 1].rect;
+        var currentDay = currentWeek[currentWeek.length - 1].$;
+        var resultString = JSON.stringify(currentDay, null, 2);
+
+        writeFile(resultString);
       });
-    });
-  }
-});
+    }
+  });
+}
+
+getData(url);
