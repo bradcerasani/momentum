b651eadc900dd40390b21e4dc6acb4c066b29b26
Brad Cerasani <bradcerasani@gmail.com>
2015-04-05 22:58:43 -0500

    Update SVG parsing to handle 53 week calendar

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var url = "https://github.com/users/" + username + "/contributions";
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     parseString(body, function(err, result) {
-      var currentWeek = result.svg.g[0].g[52].rect;
+      var calendar = result.svg.g[0].g;
+      var currentWeek = calendar[calendar.length - 1].rect;
       var currentDay = currentWeek[currentWeek.length - 1].$;
       var resultString = JSON.stringify(currentDay, null, 2);
 
